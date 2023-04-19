50b0a1c9594d02a3abe25a93a07eb15ad65bbddf
Brian Mack <kcamnairb@yahoo.com>
2023-04-19 14:38:17 -0500

    Added optional bed file output with --bed_file

--- antismash_parser.R
+++ antismash_parser.R
@@ -1,23 +1,29 @@
 #!/usr/bin/env Rscript 
-library(rvest)
-library(janitor)
-library(tidyverse)
 
-args = commandArgs(trailingOnly=TRUE)
-antismash_directory = args[1]
-output_file = args[2]
+library(rvest, quietly=TRUE)
+library(janitor, quietly=TRUE)
+library(tidyverse, quietly=TRUE)
+library(argparser, quietly=TRUE)
+
+parser = arg_parser('Parse Antismash Results into a csv or optionally a bed file')
+parser = add_argument(parser, 'antismash_directory', help='antismash directory to parse')
+parser = add_argument(parser, 'output_csv', help='output csv file')
+parser = add_argument(parser, '--bed_file', help='output bed file')
+args =  parse_args(parser)
+antismash_directory = args$antismash_directory
+output_file = args$output_csv
 parse_antismash_7 = function(antismash_directory){
   tables = read_html(paste0(antismash_directory, '/index.html')) %>% 
     html_table()
   clusters = tables %>% 
     keep(~'Region' %in% colnames(.x)) %>%
-    map(janitor::clean_names) %>%
+    map(~janitor::clean_names(.x) %>%
+          rename(backbone_type = most_similar_known_cluster_2, start=from, end=to) %>%
+          mutate(region = str_replace(region, 'Region&nbsp', 'region_')) %>%
+          mutate(across(c(start, end), ~str_remove_all(.x, ',') %>% as.numeric())) %>%
+          mutate(similarity = str_remove(similarity, '%') %>% as.numeric())) %>%
     bind_rows() %>%
-    distinct() %>%
-    rename(backbone_type = most_similar_known_cluster_2, start=from, end=to) %>%
-    mutate(region = str_replace(region, 'Region&nbsp', 'region_')) %>%
-    mutate(across(c(start, end), ~str_remove_all(.x, ',') %>% as.numeric())) %>%
-    mutate(similarity = str_remove(similarity, '%') %>% as.numeric())
+    distinct()
   cluster_blasts = list.files(paste0(antismash_directory, '/clusterblast'), full.names = T) %>%
     map(~read_lines(.x, skip=3) %>%
           tibble(text = .) %>%
@@ -41,3 +47,8 @@ parse_antismash_7 = function(antismash_directory){
 }
 antismash_results = parse_antismash_7(antismash_directory)
 antismash_results %>% write_csv(output_file, na='')
+if (! is.na(args$bed_file)){
+  antismash_results %>%
+    select(chrom, start, end, region) %>%
+    write_tsv(args$bed_file, col_names = FALSE)
+}
